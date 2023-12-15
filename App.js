8577bbfa90f81e1ece782a0617a8eb5ef4902668
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-12-15 10:09:12 +0000

    text input and switch component

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar, SafeAreaView, TextInput, Text, Switch, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [name, setName] = useState('')
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <Text style={styles.text}>My name is: {name}</Text>
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark Mode</Text>
+        <Switch value={isDarkMode} onValueChange={() => setIsDarkMode((previousState) => !previousState)}
+          trackColor={{
+            false: 'red',
+            true: 'green',
+          }}
+          thumbColor={isDarkMode ? 'blue' : 'red'}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +26,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop : StatusBar.currentHeight,
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+
+  },
+  text: {
+    fontSize: 30,
+    padding: 10,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+  }
 });
